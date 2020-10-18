8def150d41c1b20d689b23c254dbda410176451f
AlainMich <michiala@students.zhaw.ch>
2020-10-18 20:31:25 +0200

    added build and removed from gitignore

---
+++ .idea/sonarlint/issuestore/index.pb
@@ -0,0 +1,14 @@
+
+D
+src/serviceWorker.js,9\2\920ab6ab4a57ee20a36df4e3e2e73b70f9969d14
+A
+public/index.html,f\4\f43ecdf8d666ee908aacce037b4cd72159e4741b
+O
+src/components/Layout/Layout.js,a\3\a39dce0a5cc1e53785ace214d3d00a6dbf9fcabf
+<
+src/index.js,f\9\f900edc44cb929c2267a16100a32851f6d5b477d
+;
+src/App.css,a\a\aa7165aefcf8ee3d73539e99ab007daf1aa18d8d
+:
+
+src/App.js,1\e\1e43fa63aecd2127be034b3fb9d4caa172275714
---
+++ build/precache-manifest.14f767334850e4ef1abc609aa65be440.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "19386548a6c2602549c2c35d0e2f02ac",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d5b405302fa38e28a9d4",
+    "url": "/static/css/main.eab24d3c.chunk.css"
+  },
+  {
+    "revision": "d6f7011f2eae7019d36a",
+    "url": "/static/js/2.17654ab3.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/static/js/2.17654ab3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d5b405302fa38e28a9d4",
+    "url": "/static/js/main.3745ff3a.chunk.js"
+  },
+  {
+    "revision": "8db5ff64f6ddaf0c453b",
+    "url": "/static/js/runtime-main.35f63df9.js"
+  }
+]);
