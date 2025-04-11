c29f8d3ebce1d6a68a6431810e3f050008026ea3
AlainMi95 <alain.michienzi@gmx.ch>
2025-04-11 13:49:34 +0200

    Change about info

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -4,13 +4,13 @@ const About = () => (
     <section id="about">
     <h2>About Me</h2>
     <p>
-      Hey, I'm Alain — a software engineer focused on process automation. I like building practical, reliable systems that help businesses work smarter.
+      Hey, I'm Alain — a software engineer focused on process automation. I like building practical, reliable systems.
     </p>
     <p>
       I’ve worked with both frontend and backend technologies, and I enjoy figuring out how everything connects. Most of my experience is in enterprise environments, where I’ve helped streamline workflows and improve software usability.
     </p>
     <p>
-      When I’m not working, I’m usually playing guitar, out fishing, or just relaxing. I like to keep things simple, efficient, and clean — in code and in life.
+      When I’m not working, I’m usually playing guitar, out fishing, or just relaxing. 
     </p>
   </section>
 );
